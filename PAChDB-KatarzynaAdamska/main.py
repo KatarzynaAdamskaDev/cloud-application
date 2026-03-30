9856dc506ef653ee14912a3a16ab5d1f090045c8
Katarzyna A. <118213633+KatarzynaAdamskaDev@users.noreply.github.com>
2026-03-30 09:09:18 +0200

    Delete PAChDB-KatarzynaAdamska directory

--- PAChDB-KatarzynaAdamska/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()  # <- nazwa obiektu 'app' musi pasować do polecenia startowego
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
